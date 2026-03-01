fea27b82ebc57b99ba041214b6c859c41792bc5e
Thamizh <thamizh@example.com>
2026-03-01 17:31:41 +0530

    update route file

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,3 +1,13 @@
+from fastapi import APIRouter, UploadFile, File
 from app.services.summary_service import generate_rag_summary_and_quiz
 
-result = generate_rag_summary_and_quiz(text)
+router = APIRouter()
+
+@router.post("/upload")
+async def upload_notes(file: UploadFile = File(...)):
+    content = await file.read()
+    text = content.decode("utf-8")
+
+    result = generate_rag_summary_and_quiz(text)
+
+    return result
